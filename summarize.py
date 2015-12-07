e74ee036aa6b34853af4ffb3e1d37075c5a1f122
Ryan Mitchell <ryan@Ryans-MacBook-Air.local>
2015-12-07 01:10:01 -0500

    Added summarization script, data folder, timezone library

---
+++ summarize.py
@@ -0,0 +1,125 @@
+from bs4 import BeautifulSoup
+from os import listdir, walk
+from os.path import isfile, join
+import matplotlib.pyplot as plt
+import pymysql
+from config import *
+
+#Datetime stuff
+import pytz
+import datetime
+from tzwhere import tzwhere
+
+tz = None
+conn = None
+cur = None
+
+def getFirstPointTS(metaId):
+	global cur
+	global conn
+
+	cur.execute("SELECT * FROM points WHERE metaId = %s ORDER BY id ASC LIMIT 1", (metaId));
+	if cur.rowcount == 0:
+		#This timestamp doesn't have any points, apparently
+		return None
+	return cur.fetchone()['time']
+
+
+def putPointSummary(metaId, day, hour, dayofweek, latitude, longitude, enginespeed, fuelrate, vehicleSpeed):
+	global cur
+	global conn
+
+	cur.execute("SELECT * FROM point_summary WHERE day = %s AND hour = %s AND metaId = %s AND latitude = %s AND longitude = %s", (day, hour, metaId, latitude, longitude))
+	if cur.rowcount != 0:
+		print("Not unique!")
+		return
+	print("INSERT INTO point_summary(metaId, day, hour, dayofweek, latitude, longitude, enginespeed, fuelrate, vehicleSpeed) VALUES("+str(metaId)+","+str(day)+","+str(hour)+","+str(dayofweek)+","+str(latitude)+","+str(longitude)+","+str(enginespeed)+","+str(fuelrate)+","+str(vehicleSpeed)+")")
+	cur.execute("INSERT INTO point_summary(metaId, day, hour, dayofweek, latitude, longitude, enginespeed, fuelrate, vehicleSpeed) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)", (metaId, day, hour, dayofweek, latitude, longitude, enginespeed, fuelrate, vehicleSpeed))
+	conn.commit()
+
+
+def getNextPoints(metaId, lowerTime, higherTime):
+	global cur
+	global conn
+	points = []
+
+	#Make sure we don't already have these
+	cur.execute("SELECT metaId, latitude, longitude, AVG(enginespeed) AS enginespeed, AVG(fuelrate) AS fuelrate, AVG(vehicleSpeed) as vehicleSpeed FROM points WHERE metaId = %s AND time >= %s AND time < %s GROUP BY latitude, longitude", (metaId, lowerTime, higherTime))
+	if cur.rowcount == 0:
+		return None
+	return cur.fetchall()
+	
+
+def getAllMetadata():
+	global conn
+	global cur
+	cur.execute("SELECT * FROM routeMetadata")
+	return cur.fetchall()
+
+#Epoch timestamp is in *seconds* not ms
+def getLocalTimes(epochTimestamp, latitude, longitude):
+	utc_date = datetime.datetime.fromtimestamp(epochTimestamp, tz=pytz.utc)
+	print("UTC date: "+str(utc_date))
+	tzName = tz.tzNameAt(latitude, longitude);
+	print("Timezone name: "+str(tzName))
+	localtz = pytz.timezone(tzName)
+	local_date = utc_date.astimezone(localtz)
+	print("Local time: "+str(local_date))
+	weekday = local_date.weekday()
+	hour = local_date.hour
+	isoDate = local_date.isoformat()
+	return weekday, hour, isoDate
+
+
+def loadDB():
+	global conn
+	global cur
+	global tz
+	#1384210800000
+	#Just add this in here
+	tz = tzwhere.tzwhere(shapely=True, forceTZ=True)
+	conn = pymysql.connect(host=MYSQL_HOST, port=MYSQL_PORT, user=MYSQL_USER, passwd=MYSQL_PASSWORD, db='mysql', charset='utf8')
+
+	cur = conn.cursor(pymysql.cursors.DictCursor)
+	cur.execute("USE trucking")
+
+def closeDB():
+	conn.close()
+
+def summarize(points, timestamp):
+	metaId = points[0]['metaId']
+	print("Lat/long: "+str(points[0]['latitude'])+", "+str(points[0]['longitude']))
+	dayofweek, hour, isoDate = getLocalTimes(timestamp/1000, points[0]['latitude'], points[0]['longitude'])
+	print("Day of week: "+str(dayofweek)+" hour: "+str(hour)+" day: "+str(isoDate))
+	for point in points:
+		putPointSummary(metaId, isoDate, hour, dayofweek, point['latitude'], point['longitude'], point['enginespeed'], point['fuelrate'], point['vehicleSpeed'])
+
+
+#takes a metaId and returns the first timestamp, along with the 
+#second timestamp, indicating the end of the hour
+def getFirstTimestamp(metaId):
+	start = getFirstPointTS(metaId)
+	if start is None:
+		return None
+	end = start + (3600000 - start % 3600000)
+	print("End time is "+str(end))
+	return start, end
+
+loadDB()
+metadata = getAllMetadata()
+print(metadata)
+for meta in metadata:
+	times = getFirstTimestamp(meta['id'])
+	if times is not None:
+		start = times[0]
+		end = times[1]
+		points = getNextPoints(meta['id'], start, end)
+		while points is not None:
+			#Summarize and save points
+			summarize(points, int((start+end)/2))
+			start = end
+			end = start + 3600000
+			points = getNextPoints(meta['id'], start, end)
+
+
+closeDB()
--- xmlParser.py
+++ xmlParser.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
-import os
+from os import listdir, walk
+from os.path import isfile, join
 import matplotlib.pyplot as plt
 import pymysql
 from config import *
@@ -7,105 +8,186 @@ from config import *
 
 conn = None
 cur = None
+basePath = "data/"
+currentPath = "data/"
+dirList = None
+visitedList =[]
 
 def insertPoints(data):
-     global cur
-     global conn
-     #Make sure we don't already have these
-     cur.execute("SELECT * FROM points WHERE metaId = %s AND time = %s", (int(data[0][0]), int(data[0][2])))
-     print("SELECT * FROM points WHERE metaId = "+str(data[0][0])+" AND time = "+str(data[0][2]))
-     if cur.rowcount == 0:
-          print("Rowcount is ZERO!")
+	global cur
+	global conn
+	#Make sure we don't already have these
+	cur.execute("SELECT * FROM points WHERE metaId = %s AND time = %s", (int(data[0][0]), int(data[0][2])))
+	if cur.rowcount == 0:
+		cur.executemany("INSERT INTO points (metaId, time, latitude, longitude, enginespeed, fuelrate, vehicleSpeed) VALUES (%s, %s, %s, %s, %s, %s, %s)",data)
+		conn.commit()
 
-          cur.executemany("INSERT INTO points (metaId, time, latitude, longitude, enginespeed, fuelrate) VALUES (%s, %s, %s, %s, %s, %s)",data)
-          print("Executing")
-          conn.commit()
-
-     else:
-          print("This data is already inserted!")
+	else:
+		print("This data is already inserted!")
 
 
 
 def getMetaDataId(driver, truck, stamp):
-     global conn
-     global cur
-     cur.execute("SELECT * FROM routeMetadata WHERE driver = %s AND stamp = %s", (driver, int(stamp)))
-     if cur.rowcount == 0:
+	global conn
+	global cur
+	cur.execute("SELECT * FROM routeMetadata WHERE driver = %s AND stamp = %s", (driver, int(stamp)))
+	if cur.rowcount == 0:
+		cur.execute("INSERT INTO routeMetadata(driver, truck, stamp) VALUES (%s, %s, %s)", (driver, truck, stamp))
+		try:
+			conn.commit()
+			metaId = conn.insert_id()
+			return metaId
+		except:
+			conn.rollback()
 
-          cur.execute("INSERT INTO routeMetadata(driver, truck, stamp) VALUES (%s, %s, %s)", (driver, truck, stamp))
-
-          try:
-               conn.commit()
-               metaId = conn.insert_id()
-               return metaId
-          except:
-               conn.rollback()
-
-     else:
-          return cur.fetchone()['id']
+	else:
+		return cur.fetchone()['id']
 
 
 def addToDB(pathPrefix, driver, truck, stamp):
-     metaId = getMetaDataId(driver, truck, stamp)
+	i = 0
+	path = pathPrefix+"/"+str(stamp)+"-"+str(i).zfill(4)+".xml"
+	metaId = None
+	#fuelRates = []
+	#engineSpeeds = []
+	#latitudes = []
+	#longitudes = []
+	#times = []
+	data = []
+	while isfile(path):
+		f = open(path, 'r')
+		xml = f.read()
+		bsObj = BeautifulSoup(xml)
+		daes = bsObj.findAll("dae")
+		if len(daes) != 0 and metaId is None:
+				metaId = getMetaDataId(driver, truck, stamp)
+		for dae in daes:
+			fuelRate = dae.find("fr")
+			engineSpeed = dae.find("es")
+			latitude = dae.find("lat")
+			longitude = dae.find("lon")
+			vehicleSpeed = dae.find("vs")
+			time = dae.find("vne")
 
-     i = 0
-     path = pathPrefix+"/"+str(stamp)+"-"+str(i).zfill(4)+".xml"
-     print("Path is: "+path)
-     fuelRates = []
-     engineSpeeds = []
-     #latitudes = []
-     #longitudes = []
-     #times = []
-     data = []
-     while os.path.isfile(path):
-          print("Path is: "+path)
-          f = open(path, 'r')
-          xml = f.read()
-          bsObj = BeautifulSoup(xml)
-          daes = bsObj.findAll("dae")
-          for dae in daes:
-               fuelRate = dae.find("fr")
-               engineSpeed = dae.find("es")
-               latitude = dae.find("lat")
-               longitude = dae.find("lon")
-               time = dae.find("vne")
-               if(fuelRate != None and engineSpeed != None and latitude != None and longitude != None and time.attrs['time'] != None):
-                    fuelRate = float(fuelRate.get_text())
-                    engineSpeed = int(float(engineSpeed.get_text()))
-                    latitude = float(latitude.get_text())
-                    longitude = float(longitude.get_text())
-                    time = int(time.attrs['time'])
+			if(fuelRate != None and engineSpeed != None and latitude != None and longitude != None and time.attrs['time'] != None and vehicleSpeed != None):
+				fuelRate = float(fuelRate.get_text())
+				engineSpeed = int(float(engineSpeed.get_text()))
+				latitude = float(latitude.get_text())
+				longitude = float(longitude.get_text())
+				vehicleSpeed = float(vehicleSpeed.get_text())
+				time = int(time.attrs['time'])
 
-                    if(engineSpeed > 200 and fuelRate > .01):
-                         fuelRates.append(fuelRate)
-                         engineSpeeds.append(engineSpeed)
-                         #latitudes.append(latitude)
-                         #longitudes.append(longitude)
-                         #times.append(time)
-                         data.append((metaId, time, latitude, longitude, engineSpeed, fuelRate))
-          i += 1
-          path = pathPrefix+"/"+str(stamp)+"-"+str(i).zfill(4)+".xml"
-     print(data[0])
-     insertPoints(data)
+				if(vehicleSpeed > 0.3 and fuelRate > .01):
+					#fuelRates.append(fuelRate)
+					#engineSpeeds.append(engineSpeed)
+					#latitudes.append(latitude)
+					#longitudes.append(longitude)
+					#times.append(time)
+					data.append((metaId, time, latitude, longitude, engineSpeed, fuelRate, vehicleSpeed))
+		i += 1
+		path = pathPrefix+"/"+str(stamp)+"-"+str(i).zfill(4)+".xml"
+	if(len(data) > 0):
+		insertPoints(data)
+	else:
+		print("No data for leg!")
 
 
-     plt.scatter(fuelRates, engineSpeeds)
-     plt.show()
+	#plt.scatter(fuelRates, engineSpeeds)
+	#plt.show()
 
 def loadDB():
-     global conn
-     global cur
-     conn = pymysql.connect(host=MYSQL_HOST, port=MYSQL_PORT, user=MYSQL_USER, passwd=MYSQL_PASSWORD, db='mysql', charset='utf8')
+	global conn
+	global cur
+	conn = pymysql.connect(host=MYSQL_HOST, port=MYSQL_PORT, user=MYSQL_USER, passwd=MYSQL_PASSWORD, db='mysql', charset='utf8')
 
-     cur = conn.cursor(pymysql.cursors.DictCursor)
-     cur.execute("USE trucking")
+	cur = conn.cursor(pymysql.cursors.DictCursor)
+	cur.execute("USE trucking")
 def closeDB():
-     conn.close()
+	conn.close()
+
+def writeVisited(visited):
+	with open("visited.txt", "a") as f:
+		f.write(visited+"\n")
+
+def getVisited():
+	global visitedList
+	with open("visited.txt") as f:
+		visitedList = f.read().splitlines()
+
+def buildDirList():
+	global dirList
+	global basePath
+	global visitedList
+
+	print(visitedList)
+	print("Building directory list...")
+	dirList = [x[0] for x in walk(basePath) if x not in visitedList]
+	for directory in dirList:
+		if ("heartbeats" in directory):
+			dirList.remove(directory)
+		if directory in visitedList:
+			dirList.remove(directory)
+
+
+
+'''
+Returns driverName, timestamp, truckVin, from path+file-header.xml 
+'''
+def getMetaInfo(filePath):
+	truck = None
+	badVins = ["00000000000000000", "Unknown", ""]
+	with open(filePath) as f:
+		headerTxt = f.read()
+		bsObj = BeautifulSoup(headerTxt)
+		vinTag = bsObj.find("vin")
+		if vinTag is not None:
+			vin = vinTag.get_text()
+			if vin not in badVins:
+				print("Found vin tag! "+vin)
+				truck = vin
+
+	#data/pride/20150630/CliffordHuyck/20131111072506-header.xml
+	fileParts = filePath.split("/")
+	driverName = fileParts[3]
+	if "-" in driverName:
+		nameParts = driverName.split("-")
+		driverName = nameParts[1]
+	stamp = fileParts[4]
+	stamp = stamp.replace("-header.xml", "")
+	return driverName, stamp, truck
+
+def findNextDataSets():
+	global dirList
+	global visitedList
+	print("Getting list of xml files...")
+
+	fileList = []
+	newDir = None
+	while len(fileList) == 0:
+		newDir = dirList.pop(0)
+		if newDir not in visitedList:
+			print("Trying directory: "+newDir)
+			fileList = [f for f in listdir(newDir) if (isfile(join(newDir, f)) and f.endswith("-header.xml"))]
+		else:
+			print("Already visited!")
+	writeVisited(newDir)
+	print(fileList)
+	for header in fileList:
+		driverName, stamp, truck = getMetaInfo(newDir+"/"+header)
+		print("Driver name: "+driverName+", stamp: "+stamp+" truck: "+str(truck))
+		addToDB(newDir, driverName, truck, stamp)
+
+
 
 loadDB()
-pathPrefix = "data/DON-HUMMER/20151130/CandaceMarley"
-driver = "CandaceMarley"
-truck = ""
-stamp = "20151130045436"
-addToDB(pathPrefix, driver, truck, stamp)
+getVisited()
+buildDirList()
+for i in range(50):
+	findNextDataSets()
+
+#pathPrefix = "data/DON-HUMMER/20151130/CandaceMarley"
+#driver = "CandaceMarley"
+#truck = ""
+#stamp = "20151130045436"
+#addToDB(pathPrefix, driver, truck, stamp)
 closeDB()
